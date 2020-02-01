26f0c204150dbffcd43a4b68a4136e5d6aa226df
YukiNarayama <mebumitiru@gmail.com>
2020-02-01 10:40:57 +0900

    敵が動く

---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;
+    void Update ()
+    {
+        var num = Random.Range (0, 500);
+
+        if (num == 0)
+        {
+
+        }
+    }
+}
