f064fc2fbd2ad0020717377620d9506355a65605
10yen <murauehi@icloud.com>
2020-02-01 18:28:30 +0900

    敵の弾の発射音

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Shot : MonoBehaviour
 {
     [SerializeField] GameObject bulletPrefab;
+    [SerializeField] private CriAtomSource Enemy_CAS;
     [SerializeField] float shotSpeed = 500;
     [SerializeField] int shotInterval;
     void Update ()
@@ -16,6 +17,7 @@ public class Shot : MonoBehaviour
             var bullet = (GameObject) Instantiate (bulletPrefab, this.transform.position + this.transform.forward, Quaternion.identity);
             Rigidbody bulletRb = bullet.GetComponent<Rigidbody> ();
             bulletRb.AddForce (transform.forward * shotSpeed);
+            Enemy_CAS.Play();
         }
     }
 }
