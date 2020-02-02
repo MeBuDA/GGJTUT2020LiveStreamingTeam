00e50734b4fda789ce4b810810401cfade1e5c31
10yen <murauehi@icloud.com>
2020-02-02 13:50:03 +0900

    Font

--- Assets/Scripts/MinoReset.cs
+++ Assets/Scripts/MinoReset.cs
@@ -1,23 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MinoReset : MonoBehaviour
 {
     [SerializeField] GameObject[] Mino = new GameObject[5];
     [SerializeField] StartCubeSet CubeSet;
-
-    public void MinoReSet()
+    [SerializeField] Text text;
+    public static int score = 0;
+    public void MinoReSet ()
     {
-        CubeSet.CubeReset();
-        MatoSet();
+        CubeSet.CubeReset ();
+        MatoSet ();
+        score += 100;
+        text.text = "スコア : " + score;
     }
-    public void MatoSet()
+    public void MatoSet ()
     {
         for (int i = 0; i < Mino.Length; i++)
         {
-            Mino[i].SetActive(false);
+            Mino[i].SetActive (false);
         }
-        Mino[Random.Range(0,4)].SetActive(true);
+        Mino[Random.Range (0, 4)].SetActive (true);
     }
 }
